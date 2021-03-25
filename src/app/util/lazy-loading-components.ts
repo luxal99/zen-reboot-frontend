33ba100e42b6968272523ff49afeef85e99966a4
Aleksa <aleksa.lukic.3659@metropolitan.ac.rs>
2021-03-25 11:01:54 +0100

    Create lazy-loading-component util

---
+++ src/app/util/lazy-loading-components.ts
@@ -0,0 +1,11 @@
+import {ComponentFactoryResolver, ComponentRef, ViewContainerRef} from '@angular/core';
+
+import {ComponentType} from '@angular/cdk/portal';
+
+export class LazyLoadComponentsUtil {
+  static loadComponent(component: ComponentType<any>, entry: ViewContainerRef, resolver: ComponentFactoryResolver): ComponentRef<any> {
+    entry.clear();
+    const factory = resolver.resolveComponentFactory(component);
+    return entry.createComponent(factory);
+  }
+}
