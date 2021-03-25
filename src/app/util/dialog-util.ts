d63d6b5e1b253fd5343362791fd008cc7dbf11e2
Aleksa <aleksa.lukic.3659@metropolitan.ac.rs>
2021-03-25 11:04:35 +0100

    Create dialog-util

---
+++ src/app/util/dialog-util.ts
@@ -0,0 +1,9 @@
+import {ComponentType} from '@angular/cdk/portal';
+import {MatDialog, MatDialogConfig, MatDialogRef} from '@angular/material/dialog';
+
+export class DialogUtil {
+  static openDialog(component: ComponentType<any>, options: MatDialogConfig, dialog: MatDialog): MatDialogRef<any> {
+    return dialog.open<any>(component, options);
+
+  }
+}
