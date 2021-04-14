d9542af0684accd888fdee7361caa943918fa254
Aleksa <aleksa.lukic.3659@metropolitan.ac.rs>
2021-04-14 10:34:06 +0200

    Clean code

--- src/app/components/admin/component-activator/component-activator.component.ts
+++ src/app/components/admin/component-activator/component-activator.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit, Output} from '@angular/core';
+import {EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-component-activator',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComponentActivatorComponent implements OnInit {
 
-  constructor() { }
+  // tslint:disable-next-line:no-output-native
+  @Output() load = new EventEmitter();
+
+  @Input() icon = '';
+  @Input() title = '';
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  changeColor(forwardedElement: any): void {
+    const element = document.querySelectorAll('.active');
+    [].forEach.call(element, (el: any) => {
+      el.classList.remove('active');
+    });
+    const elementWithClassList: HTMLElement[] = forwardedElement.path.filter((x: HTMLElement) => x.classList !== undefined);
+    for (const mainElement of elementWithClassList) {
+      mainElement.classList.forEach(className => {
+        if (className === 'a-h2') {
+          mainElement.classList.add('active');
+        }
+      });
+    }
+  }
+
+  loadComponent(): void {
+    this.load.emit(true);
+  }
 }
