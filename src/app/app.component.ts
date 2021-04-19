83fbbaa17ccb6771d5dcd01cc4d6e12befb1a152
Aleksa <aleksa.lukic.3659@metropolitan.ac.rs>
2021-04-19 18:09:45 +0200

    Add animation on header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+// @ts-ignore
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'zen-frontend';
+
+  ngOnInit(): void {
+    AOS.init();
+  }
 }
