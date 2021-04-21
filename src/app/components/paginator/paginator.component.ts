cf1b77e9a472f8d16fcfee3e144db15b56cde50f
Aleksa <aleksa.lukic.3659@metropolitan.ac.rs>
2021-04-21 11:10:30 +0200

    .

--- src/app/components/paginator/paginator.component.ts
+++ src/app/components/paginator/paginator.component.ts
@@ -1,5 +1,4 @@
-import {Component, Input, OnInit, Output} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Component, OnInit, Output} from '@angular/core';
 import {EventEmitter} from '@angular/core';
 
 @Component({
@@ -10,53 +9,13 @@ import {EventEmitter} from '@angular/core';
 export class PaginatorComponent implements OnInit {
 
 
-  @Input() gap = 10;
-  @Input() filteredList: any[] = [];
-  @Input() response!: Observable<any[]>;
-  @Output() output = new EventEmitter();
-  initGap = 0;
-  isDisabledPrev10: any;
-  isDisabledNext10: any;
+  @Output() next = new EventEmitter();
+  @Output() prev = new EventEmitter();
 
   constructor() {
   }
 
   ngOnInit(): void {
-  }
-
-  nextStaffs(): void {
-    this.response.subscribe((resp) => {
-      if (this.initGap + 10 < resp.length) {
-        this.initGap += 10;
-        this.gap += 10;
-        this.isDisabledPrev10 = false;
-      } else {
-        this.isDisabledNext10 = true;
-      }
-    });
-
-    this.response.subscribe((resp) => {
-      this.filteredList = resp.slice(this.initGap, this.gap);
-      this.output.emit(this.filteredList);
-
-      console.log(this.filteredList);
-    });
-  }
-
-  previousStaffs(): void {
-    if (this.initGap - 10 >= 0) {
-      this.initGap -= 10;
-      this.isDisabledNext10 = false;
-    } else {
-      this.isDisabledPrev10 = true;
-    }
-    if (this.gap - 10 >= 10) {
-      this.gap -= 10;
-    }
-    this.response.subscribe((resp) => {
-      this.filteredList = resp.slice(this.initGap, this.gap);
-      this.output.emit(resp.slice(this.initGap, this.gap));
-    }).unsubscribe();
   }
 
 }
