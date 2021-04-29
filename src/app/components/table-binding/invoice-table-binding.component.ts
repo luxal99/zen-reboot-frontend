dac119ae89c8fea069a931842225066db9c5176a
Aleksa <aleksa.lukic.3659@metropolitan.ac.rs>
2021-04-29 11:21:09 +0200

    Open invoice method

--- src/app/components/table-binding/invoice-table-binding.component.ts
+++ src/app/components/table-binding/invoice-table-binding.component.ts
@@ -1,4 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {DialogUtil} from '../../util/dialog-util';
+import {InvoicesDialogOverviewComponent} from './invoices-dialog-overview/invoices-dialog-overview.component';
+import {Invoice} from '../../models/invoice';
 
 @Component({
   selector: 'app-table-binding',
@@ -10,10 +14,18 @@ export class InvoiceTableBindingComponent implements OnInit {
   displayedColumns: string[] = ['client', 'billedClient', 'location', 'gross', 'option'];
   @Input() dataSource: any[] = [];
 
-  constructor() {
+  constructor(private dialog: MatDialog) {
   }
 
   ngOnInit(): void {
   }
 
+  openInvoiceDialogOverview(data: Invoice): void {
+    DialogUtil.openDialog(InvoicesDialogOverviewComponent, {
+      height: '100%',
+      width: '40%',
+      position: {right: '0'},
+      data
+    }, this.dialog);
+  }
 }
