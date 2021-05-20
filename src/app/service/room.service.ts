07c46d4afea398a0f3cdbaf247f21879c68528ab
Aleksa <aleksa.lukic.3659@metropolitan.ac.rs>
2021-05-20 16:23:01 +0200

    Add service for get all rooms with app

--- src/app/service/room.service.ts
+++ src/app/service/room.service.ts
@@ -2,10 +2,15 @@ import {Injectable} from '@angular/core';
 import {GenericService} from './generic.service';
 import {Room} from '../models/room';
 import {RestRoutesConst} from '../const/const';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomService extends GenericService<Room> {
   route = RestRoutesConst.ROOM;
+
+  getAppointmentsForAllRooms(): Observable<any[]> {
+    return this.http.get<any[]>(RestRoutesConst.API + this.route + '/rooms/appointments', {responseType: 'json'});
+  }
 }
