a8b2957889926fe57876e3b32124a02b939027e1
Aleksa <aleksa.lukic.3659@metropolitan.ac.rs>
2021-05-26 16:55:20 +0200

    Package model

--- src/app/models/package.ts
+++ src/app/models/package.ts
@@ -1,3 +1,19 @@
+import {Client} from './client';
+import {PaymentMethod} from './payment-method';
+
 export interface Package {
-  
+  id?: number;
+  availablePrice?: number;
+  availableUsages?: number;
+  client?: Client;
+  code?: string;
+  discount?: number;
+  discountType?: string;
+  startDate?: string;
+  expired?: string;
+  location?: Location;
+  paymentMethod?: PaymentMethod;
+  price?: string;
+  treatmentPrice: number;
+  treatmentName?: string;
 }
