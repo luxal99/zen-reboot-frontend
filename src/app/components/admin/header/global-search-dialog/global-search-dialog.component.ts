24cedf928aff0d991a622083f11acb3c22e6a864
Aleksa <aleksa.lukic.3659@metropolitan.ac.rs>
2021-06-02 11:08:45 +0200

    Appointment search

--- src/app/components/admin/header/global-search-dialog/global-search-dialog.component.ts
+++ src/app/components/admin/header/global-search-dialog/global-search-dialog.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 import {ClientService} from '../../../../service/client.service';
 import {AppointmentService} from '../../../../service/appointment.service';
+import {Client} from '../../../../models/client';
+import {CriteriaBuilder} from '../../../../util/criteria-builder';
+import {FormControlNames} from '../../../../const/const';
 
 @Component({
   selector: 'app-global-search-dialog',
@@ -11,6 +14,8 @@ import {AppointmentService} from '../../../../service/appointment.service';
 export class GlobalSearchDialogComponent implements OnInit {
 
   searchText = '';
+  listOfClients: Client[] = [];
+  listOfAppointments: Client[] = [];
 
   searchForm = new FormGroup({
     search: new FormControl('')
@@ -31,14 +36,50 @@ export class GlobalSearchDialogComponent implements OnInit {
 
       // @ts-ignore
       document.getElementById('centered').style.minHeight = '70vh';
-    } else {
+      this.listOfClients = [];
+      this.listOfAppointments = [];
+    } else if (this.searchText.length > 1) {
       // @ts-ignore
       document.getElementById('search').style.display = 'block';
       // @ts-ignore
       document.getElementById('huge-label').style.display = 'none';
       // @ts-ignore
       document.getElementById('centered').style.minHeight = '30vh';
+      this.searchClient();
+      this.searchAppointment();
+
     }
+  }
+
+  searchClient(): void {
+    const queryBuilder = new CriteriaBuilder();
+    const search: string = this.searchText;
+    queryBuilder.startsWith('person.firstName', search).or()
+      .startsWith('person.contacts.value', search);
+    queryBuilder.criteriaList = queryBuilder.criteriaList.filter((searchCriteria) => searchCriteria.secondOperand !== '');
+    if (search.length > 1) {
+      this.clientService.getAllSearchByQueryParam(queryBuilder.buildUrlEncoded())
+        .pipe()
+        .subscribe((resp) => {
+          this.listOfClients = resp;
+        });
+    } else if (search.length === 0) {
+
+    }
+  }
+
+  searchAppointment(): void {
+    const queryBuilder = new CriteriaBuilder();
+    queryBuilder.startsWith('staff.person.firstName', this.searchText).or()
+      .startsWith('staff.person.lastName', this.searchText).or()
+      .startsWith('clients.person.firstName', this.searchText).or()
+      .startsWith('clients.person.lastName', this.searchText).or()
+      .startsWith('treatmentDuration.treatment.name', this.searchText).or();
+
+    queryBuilder.criteriaList = queryBuilder.criteriaList.filter((searchCriteria) => searchCriteria.secondOperand !== '');
+    this.appointmentService.getAll(queryBuilder.buildUrlEncoded()).subscribe((resp) => {
+      this.listOfAppointments = resp;
+    });
   }
 
   showSearch(): void {
