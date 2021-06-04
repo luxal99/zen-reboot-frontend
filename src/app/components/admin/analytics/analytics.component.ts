2c9868dc85df7e21d5ae6f188ef3132eb6909dd2
Aleksa <aleksa.lukic.3659@metropolitan.ac.rs>
2021-06-04 12:10:37 +0200

    Write analytics service for get expired packages

--- src/app/components/admin/analytics/analytics.component.ts
+++ src/app/components/admin/analytics/analytics.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AnalyticsService} from 'src/app/service/analytics.service';
 
 @Component({
   selector: 'app-analytics',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnalyticsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private analyticsService: AnalyticsService) {
+  }
 
   ngOnInit(): void {
   }
--- src/app/service/analytics.service.ts
+++ src/app/service/analytics.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {RestRoutesConst} from '../const/const';
+import {Package} from '../models/package';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +16,7 @@ export class AnalyticsService {
     return this.http.get<string[]>(RestRoutesConst.API + RestRoutesConst.ANALYTICS + '/periods', {responseType: 'json'});
   }
 
-  // getExpiredPackages(): Observable<any> {
-  //
-  // }
+  getExpiredPackages(period: string): Observable<Package[]> {
+    return this.http.get<Package[]>(RestRoutesConst.API + RestRoutesConst.ANALYTICS + '/expired?=' + period, {responseType: 'json'});
+  }
 }
