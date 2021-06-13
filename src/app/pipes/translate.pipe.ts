6b91ebb403a93fb104217df9fd28157eb6b5707c
Aleksa <aleksa.lukic.3659@metropolitan.ac.rs>
2021-06-13 10:37:38 +0200

    Translate dropdowns

--- src/app/pipes/translate.pipe.ts
+++ src/app/pipes/translate.pipe.ts
@@ -1,12 +1,49 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
   name: 'translate'
 })
 export class TranslatePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string): string {
+    const valueToUpperCase = value.toUpperCase();
+    if (!value) {
+      return '';
+    }
+
+    if (valueToUpperCase === 'PERCENT') {
+      return 'Procenat';
+    } else if (valueToUpperCase === 'FLAT') {
+      return 'Fiksno';
+    } else if (valueToUpperCase === 'NONE') {
+      return 'Nijedan';
+    } else if (valueToUpperCase === 'CASH') {
+      return 'Keš';
+    } else if (valueToUpperCase === 'CARD') {
+      return 'Kartica';
+    } else if (valueToUpperCase === 'ACCOUNT PAYMENT') {
+      return 'Plaćanje preko računa';
+    } else if (valueToUpperCase === 'DISCOUNT') {
+      return 'Popust';
+    } else if (valueToUpperCase === 'VOUCHER') {
+      return 'Vaučer';
+    } else if (valueToUpperCase === 'ONLINE VOUCHER') {
+      return 'Online vaučer';
+    } else if (valueToUpperCase === 'PACKAGE') {
+      return 'Paket';
+    } else if (valueToUpperCase === 'POINT OF SALE') {
+      return 'Na licu mesta';
+    } else if (valueToUpperCase === 'NEW') {
+      return 'Novi';
+    } else if (valueToUpperCase === 'CANCELED') {
+      return 'Otkazan';
+    } else if (valueToUpperCase === 'COMPLETED') {
+      return 'Kompletiran';
+    } else if (valueToUpperCase === 'CONFIRMED') {
+      return 'Potvrđen';
+    } else {
+      return value;
+    }
   }
 
 }
