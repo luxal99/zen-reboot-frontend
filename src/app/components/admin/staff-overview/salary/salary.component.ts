6663a2fc063b6a2035fc0f531b009b83f6d82020
Aleksa Lukic <lukicaleksa04@gmail.com>
2021-07-27 17:04:46 +0200

    Lazy loading tabs

---
+++ src/app/components/admin/staff-overview/salary/salary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-salary',
+  templateUrl: './salary.component.html',
+  styleUrls: ['./salary.component.sass']
+})
+export class SalaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/admin/staff-overview/staff/staff.component.ts
@@ -0,0 +1,48 @@
+import {Component, OnInit, ViewChild} from "@angular/core";
+import {DefaultComponent} from "../../../../util/default-component";
+import {Staff} from "../../../../models/staff";
+import {MatSpinner} from "@angular/material/progress-spinner";
+import {FormControl, FormGroup} from "@angular/forms";
+import {MatDialog} from "@angular/material/dialog";
+import {StaffService} from "../../../../service/staff.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {RoleSettings} from "../../../../const/const";
+import {DialogUtil} from "../../../../util/dialog-util";
+import {AddStaffDialogComponent} from "../add-staff-dialog/add-staff-dialog.component";
+import {setDialogConfig} from "../../../../util/dialog-options";
+
+@Component({
+  selector: "app-staff",
+  templateUrl: "./staff.component.html",
+  styleUrls: ["./staff.component.sass"]
+})
+export class StaffComponent extends DefaultComponent<Staff> implements OnInit {
+
+  @ViewChild("spinner") spinner!: MatSpinner;
+
+  searchForm = new FormGroup({
+    search: new FormControl("")
+  });
+
+  searchText = "";
+
+  constructor(private dialog: MatDialog, private staffService: StaffService,
+              protected snackBar: MatSnackBar, public roleSetting: RoleSettings) {
+    super(staffService, snackBar);
+  }
+
+  ngOnInit(): void {
+    super.ngOnInit();
+  }
+
+  openAddStaffDialog(data?: Staff): void {
+    DialogUtil.openDialog(AddStaffDialogComponent, setDialogConfig({
+      height: "80%",
+      width: "40%",
+      data
+    }), this.dialog).afterClosed().subscribe(() => {
+      this.getItems();
+    });
+  }
+
+}
