93cdc1842be373ff5d03f4e4101bfc5a1d4f9fab
Aleksa Lukic <lukicaleksa04@gmail.com>
2021-07-28 16:50:48 +0200

    Create cancel-appointment-dialog

---
+++ src/app/components/admin/appointment/appointment-overview-dialog/cancel-appointment-dialog/cancel-appointment-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cancel-appointment-dialog',
+  templateUrl: './cancel-appointment-dialog.component.html',
+  styleUrls: ['./cancel-appointment-dialog.component.sass']
+})
+export class CancelAppointmentDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
